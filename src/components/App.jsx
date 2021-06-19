273b6a06df94d1f7e5db4fca8530ef42dd13c750
discovernow <taashigoyal17@gmail.com>
2021-06-19 21:34:08 +0530

    Adding greeting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,58 @@
 import React from "react";
 
 function App() {
+
+  const [title, setTitle] = React.useState("Hello There");
+  const [name, setName] = React.useState("");
+  const [isMouseOver, buttonOver] = React.useState(false);
+  const date = new Date();
+  var greeting = "";
+
+  function changeHandler(event) {
+    setName(event.target.value);
+  }
+  function mouseHandler(){
+    buttonOver(true);
+  }
+  function mouseOutHandler(){
+    buttonOver(false);
+  }
+
   function getTime() {
     document.querySelector("button").style.visibility = "hidden";
+    document.querySelector("input").style.visibility = "hidden";
     setInterval(() => {
       setTime(new Date().toLocaleTimeString());
     }, 1000);
+    if (date.getHours >= 12 && date.getHours < 16) {
+      greeting = "Good Afternoon ";
+    } else if(date.getHours >= 16 && date.getHours <= 20){
+      greeting = "Good Evening ";
+    } else if(date.getHours >= 4 && date.getHours < 12){
+      greeting = "Good Morning ";
+    } else {
+      greeting = "Good Night ";
+    }
+    setTitle(greeting + name);
   }
 
   const [TIME, setTime] = React.useState("CLOCK");
 
   return (
     <div className="container">
+    <h1>{title}</h1>
       <h1>{TIME}</h1>
-      <button onClick={getTime}>Show Time</button>
+      <input
+        onChange={changeHandler}
+        value={name}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button 
+      style={{backgroundColor: isMouseOver? "black": "white" }}
+      onMouseOver={mouseHandler}
+      onMouseOut={mouseOutHandler}
+      onClick={getTime}>Show Time</button>
     </div>
   );
 }
