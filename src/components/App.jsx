7187426ff75b6ef346c39d74e7099056bfe27686
discovernow <taashigoyal17@gmail.com>
2021-06-19 22:37:01 +0530

    Modify Appearance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 function App() {
-
   const [title, setTitle] = React.useState("Hello There");
   const [name, setName] = React.useState("");
+  const [time, setTime] = React.useState("Unlock your Clock");
   const [isMouseOver, buttonOver] = React.useState(false);
   const date = new Date();
   var greeting = "";
+  var color = "#CEE5D0";
 
   function changeHandler(event) {
     setName(event.target.value);
   }
-  function mouseHandler(){
+  function mouseHandler() {
     buttonOver(true);
   }
-  function mouseOutHandler(){
+  function mouseOutHandler() {
     buttonOver(false);
   }
 
@@ -26,33 +27,40 @@ function App() {
     }, 1000);
     if (date.getHours >= 12 && date.getHours < 16) {
       greeting = "Good Afternoon ";
-    } else if(date.getHours >= 16 && date.getHours <= 20){
+      color = "#FAF1E6";
+    } else if (date.getHours >= 16 && date.getHours <= 20) {
       greeting = "Good Evening ";
-    } else if(date.getHours >= 4 && date.getHours < 12){
+      color = "#5E8B7E";
+    } else if (date.getHours >= 4 && date.getHours < 12) {
       greeting = "Good Morning ";
+      color = "#F9DFDC";
     } else {
       greeting = "Good Night ";
+      color = "black";
+      document.querySelector("body").style.color = "white";
     }
+    document.querySelector("body").style.background = color;
     setTitle(greeting + name);
   }
 
-  const [TIME, setTime] = React.useState("CLOCK");
-
   return (
     <div className="container">
-    <h1>{title}</h1>
-      <h1>{TIME}</h1>
+      <h1>{title}</h1>
+      <p>{time}</p>
       <input
         onChange={changeHandler}
         value={name}
         type="text"
         placeholder="What's your name?"
       />
-      <button 
-      style={{backgroundColor: isMouseOver? "black": "white" }}
-      onMouseOver={mouseHandler}
-      onMouseOut={mouseOutHandler}
-      onClick={getTime}>Show Time</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={mouseHandler}
+        onMouseOut={mouseOutHandler}
+        onClick={getTime}
+      >
+        Show Time
+      </button>
     </div>
   );
 }
